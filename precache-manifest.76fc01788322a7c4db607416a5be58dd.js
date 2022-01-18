87433bf89c696e378a15677131a136c95e6c1527
ghaithdiab <ghaith.diab@hotmail.com>
2022-01-18 11:26:13 +0100

    Updates

---
+++ precache-manifest.76fc01788322a7c4db607416a5be58dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c7f5277c063576954372f7f80561f0",
+    "url": "/ghaithdiab_11_10012022/index.html"
+  },
+  {
+    "revision": "4cc49893e05e1b5e8bc9",
+    "url": "/ghaithdiab_11_10012022/static/css/main.ceb7c5f4.chunk.css"
+  },
+  {
+    "revision": "45347c3df784aa31f91e",
+    "url": "/ghaithdiab_11_10012022/static/js/2.6c2589cc.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/ghaithdiab_11_10012022/static/js/2.6c2589cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc49893e05e1b5e8bc9",
+    "url": "/ghaithdiab_11_10012022/static/js/main.58d2df45.chunk.js"
+  },
+  {
+    "revision": "820764bdd3b5f17b6647",
+    "url": "/ghaithdiab_11_10012022/static/js/runtime-main.a4c29e32.js"
+  },
+  {
+    "revision": "c3e7b3ebd123e6fdfe3bd71f5256b463",
+    "url": "/ghaithdiab_11_10012022/static/media/Background.c3e7b3eb.png"
+  },
+  {
+    "revision": "454a98c9b11e0eec753b6024a870fabe",
+    "url": "/ghaithdiab_11_10012022/static/media/BackgroundAboutPage.454a98c9.png"
+  },
+  {
+    "revision": "61035ad50485d924b4d0f299ac40642b",
+    "url": "/ghaithdiab_11_10012022/static/media/leftBtn.61035ad5.svg"
+  },
+  {
+    "revision": "b6a25521fa793a0eb748ea85cd6f44e6",
+    "url": "/ghaithdiab_11_10012022/static/media/rightBtn.b6a25521.svg"
+  },
+  {
+    "revision": "1b740f08b95305407f3262e39b684c82",
+    "url": "/ghaithdiab_11_10012022/static/media/star.1b740f08.svg"
+  },
+  {
+    "revision": "76999bbf056f7dbc34f006a0f5cefa01",
+    "url": "/ghaithdiab_11_10012022/static/media/starColor.76999bbf.svg"
+  }
+]);
